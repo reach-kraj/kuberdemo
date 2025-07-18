dfed5aead8bae2495be64a7f2e2a785eaefc1ba9
Karthickraj Gangadharan <kraj.g@infograss.com>
2025-07-18 13:30:40 +0530

    commit v12

--- src/main/java/com/demo/kuberdemo/controller/UserController.java
+++ src/main/java/com/demo/kuberdemo/controller/UserController.java
@@ -8,7 +8,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api")
 public class UserController {
-//intial test commit
+//intial test 
     @GetMapping("/hello")
     public Map<String, String> hello() {
         return Map.of(
